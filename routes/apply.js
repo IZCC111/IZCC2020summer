8e203ef1d2b5333aca769c2c40f08a24ad9e0de6
ItisCaleb <40360126+ItisCaleb@users.noreply.github.com>
2020-06-18 20:54:03 +0800

    add email send

--- routes/apply.js
+++ routes/apply.js
@@ -4,17 +4,7 @@ const nodeMailer = require('nodemailer');
 const ejs = require('ejs');
 
 
-/*const transporter = nodeMailer.createTransport({
-    service:'gmail',
-    auth: {
-        type:'OAuth2',
-        user: process.env.MAIL,
-        pass:process.env.MAILPASSWORD,
-        clientId:'480631511990-gvng29rvk7c832t1siepdai4m40qbda4.apps.googleusercontent.com',
-        clientSecret:'Jii_L5XlQ4XsqCIaMXji-c39',
-        refreshToken:'1//04Exl2_AUU7E_CgYIARAAGAQSNwF-L9IrPqNgA0M14nNtW4z77C2RRWvc16jLAK32KjGdNlWst3FcwlW5_4wJgJrSXiOZMtGupvI',
-    },
-});*/
+
 
 router.post('/register', async function(req, res) {
     const form =req.body;
@@ -34,20 +24,33 @@ router.post('/register', async function(req, res) {
         note:form.note,
     });
     try{
+        const transporter = nodeMailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            auth: {
+                type:'OAuth2',
+                user:process.env.MAIL,
+                pass:process.env.MAILPASSWORD,
+                clientId:'480631511990-gvng29rvk7c832t1siepdai4m40qbda4.apps.googleusercontent.com',
+                clientSecret:'Jii_L5XlQ4XsqCIaMXji-c39',
+                refreshToken:'1//04_-2N-UG9BlrCgYIARAAGAQSNwF-L9IrS2BJmRjF7DbvLxNMyPwRbo8wOLZE2fl7yYKJuztGwuXOaq9yAOcC8DztngqySGtQGpI',
+            },
+        });
         await formInfo.save();
-        /*ejs.renderFile(__dirname+'/../views/applymail.ejs',
+        ejs.renderFile(__dirname+'/../views/applymail.ejs',{name:formInfo.name},
             (err,html)=>{
             const mail ={
                 from:'楓下共建成中景 <izcc111st@gmail.com>',
                 to:formInfo.email,
                 subject:'報名成功通知',
-                text:'testing'
+                html:html
             }
             transporter.sendMail(mail,(err,info)=>{
                 if (err) console.log(err);
                 else console.log(info);
             });
-        })*/
+        })
         res.send('報名成功!請隨時關注我們的粉專來確定活動消息!');
     }catch (e) {
         console.log(e)
@@ -60,5 +63,9 @@ router.post('/register', async function(req, res) {
 router.get('/admit/:id',async function (req,res) {
     await apply.updateOne({_id:req.params.id},{$set:{success:req.query.value}})
     res.send('success')
+});
+router.get('/delete/:id',async function (req,res) {
+    await apply.deleteOne({_id:req.params.id})
+    res.send('deleted')
 });
 module.exports=router;
